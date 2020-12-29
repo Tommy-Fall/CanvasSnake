9385953907d7a64e0bdc32a5baf160770f8b61f7
Cawilo <finished92for@gmail.com>
2020-12-28 21:39:44 -0500

    efe

--- script.js
+++ script.js
@@ -4,18 +4,17 @@ canvas.width = 500
 canvas.height = 500
 
 snake = {
-    points: 0,
+    points: 1,
     x: 250,
     y: 250,
     width: 25,
     height: 25,
     direction: 0,
     speed: 25,
-    tail: [[], []]
+    tail: []
 
 }
 
-// snake.tail[0] = {x: snake.x, y:snake.y}
 
 
 
@@ -42,28 +41,28 @@ drawSnake = () => {
 
     if (snake.direction === 1) {
 
-        if (snake.y !== 0) {
+        if (snake.y !== -25) {
             snake.y -= snake.speed
             checkTail()
         }
 
     }
     if (snake.direction === 2) {
-        if (snake.x !== 475) {
+        if (snake.x !== 500) {
             snake.x += snake.speed
             checkTail()
         }
 
     }
     if (snake.direction === 3) {
-        if (snake.y !== 475) {
+        if (snake.y !== 500) {
             snake.y += snake.speed
             checkTail()
         }
 
     }
     if (snake.direction === 4) {
-        if (snake.x !== 0) {
+        if (snake.x !== -25) {
             snake.x -= snake.speed
             checkTail()
         }
@@ -72,7 +71,6 @@ drawSnake = () => {
 
     if (snake.y === food.y && snake.x === food.x) {
         snake.points += 1
-        //snake.tail = []
         changeFoodLocation()
     }
     ctx.fillStyle = 'red'
@@ -83,12 +81,35 @@ drawSnake = () => {
 }
 
 checkTail = () => {
-    
-    
+    snake.tail.unshift([snake.x,snake.y])
+    snake.tail.length = snake.points
+    for(let i = 0; i<snake.points; i++){
+        ctx.fillStyle = 'red'
+        ctx.fillRect(snake.tail[i][0], snake.tail[i][1], snake.width, snake.height);
+    }
+
+    for(let i = 0; i<snake.tail.length; i++){
+        if(snake.tail[i][0] == snake.x && snake.tail[i][1] == snake.y && i !== 0){
+            resetGame()
+        }
+    }
+
+    if(snake.y === 500 || snake.y === -25 || snake.x === 500 || snake.x === -25){
+        resetGame()
+    }
+}
+
+
+resetGame = () => {
+    snake.tail = []
+    snake.points = 1
+    snake.direction = 0
+    snake.x = 250,
+    snake.y = 250
 }
 
 window.addEventListener('keydown', () => {
-    //console.log(event.keyCode)
+    
 
 
     if (event.keyCode === 87 && snake.direction !== 3) {
@@ -112,7 +133,7 @@ drawFood = () => {
 }
 
 drawScore = () => {
-    ctx.strokeText('score: '+snake.points, 420, 25);
+    ctx.strokeText('score: '+(Number(snake.points)-1), 420, 25);
 }
 
 changeFoodLocation = () => {
@@ -121,7 +142,6 @@ changeFoodLocation = () => {
         let number = Math.floor(Math.random() * 475+1);
         if ((number % 25) === 0 || number === 0) {
             food.x = number
-
             break
         }
     }
@@ -129,7 +149,6 @@ changeFoodLocation = () => {
         let number = Math.floor(Math.random() * 475+1);
         if ((number % 25) === 0 || number === 0) {
             food.y = number
-
             break
         }
     }
@@ -149,13 +168,13 @@ startAnimating = (fps) => {
 
 
 animate = () => {
-
+    
     requestAnimationFrame(animate)
     now = Date.now();
     elapsed = now - then;
     if (elapsed > fpsInterval) {
         then = now - (elapsed % fpsInterval)
-        //console.log('123')
+        
 
 
 
@@ -167,9 +186,9 @@ animate = () => {
         drawScore()
         
     }
-    //ctx.clearRect(0, 0, canvas.width, canvas.height)
+    
 
 }
 
 
-startAnimating(5)
+startAnimating(7)
